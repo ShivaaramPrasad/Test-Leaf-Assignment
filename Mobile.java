ccea3a75f80ba961a9281124c8ab0edc4a25a52d
shivaaram25 <53117301+shivaaram25@users.noreply.github.com>
2020-03-31 11:28:10 +0530

    Week1.day1

---
+++ Mobile.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Mobile {
+ String mobBrand="Oneplus";
+ boolean mobfresh=true;
+ int mobage=1;
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("I am ready with java");
+ Mobile m= new Mobile();
+int mobage2=m.mobage;
+boolean mobfresh2=m.mobfresh;
+String mobBrand2=m.mobBrand;
+System.out.println(mobage2);
+System.out.println(mobfresh2);
+System.out.println(mobBrand2);
+
+	}
+
+}
